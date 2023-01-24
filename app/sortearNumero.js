9030f658269163b43aa9494c2664d64773f92753
Bruno Pocay <brunopocay@gmail.com>
2023-01-24 18:07:26 -0300

    finalizando projeto

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,5 +1,5 @@
 const menorValor = 1
-const maiorValor = 1000
+const maiorValor = 999
 const numeroSecreto = gerarNumeroAleatório()
 
 function gerarNumeroAleatório(){
---
+++ app/validacao.js
@@ -0,0 +1,44 @@
+function verificaValorValido(chute){
+    const numero = +chute 
+
+    if (chuteInvalido(numero)){
+        elementoChute.innerHTML += '<div> Valor Inválido </div>'
+        return
+    }
+
+    if (verificarNumero(numero)){
+        elementoChute.innerHTML += `<div>Valor inválido: Fale um numero entre ${menorValor} e ${maiorValor}</div>
+        `
+        return
+    }
+
+    if(numero === numeroSecreto){
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O numero secreto era ${numeroSecreto}</h3>
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `
+    } else if(numero > numeroSecreto){
+        elementoChute.innerHTML += `<div>O numero secreto é menor <i class="fa-solid fa-circle-down"></i></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `<div>O numero secreto é maior <i class="fa-solid fa-circle-up"></i></i></div>
+        `
+    }
+    
+
+}
+
+function chuteInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function verificarNumero(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e =>{
+    if(e.target.id == 'jogar-novamente'){
+        window.location.reload()
+    }
+})
